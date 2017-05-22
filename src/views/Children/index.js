004a2a01f9ad5e1f422b3a267b5549e4d5201c8a
Saturn226 <serenehopes@gmail.com>
2017-05-22 01:14:52 -0400

    childrens page successfully fetches data from api/children

--- src/views/Children/index.js
+++ src/views/Children/index.js
@@ -1,3 +1,30 @@
 import React from 'react'
-const Children = () => <div>List of All Children</div>
-module.exports = Children
+
+
+export default class Children extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      children: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('api/children')
+      .then(response => response.json())
+      .then(data => this.setState({
+        children: data
+      }))
+  }
+  render(){
+    const children = this.state.children.map((child) =>{
+      return (<li key={child.id}> {child.name} {child.notes}</li>)
+    })
+    return(
+      <div>
+        List of All Children
+        <ul>{children}</ul>
+      </div>
+    )
+  }
+}
